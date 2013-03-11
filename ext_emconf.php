ec52ea13cec26d33ba6af036bfbfacbdbc393f91
sfsmfc <sfsmfc@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-03-11 14:25:06 +0000

    update dependencies
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/static_info_tables_pl/trunk@72652 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,9 +34,9 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'static_info_tables' => '2.3.0-',
-			'php' => '4.1.0-0.0.0',
-			'typo3' => '4.5-0.0.0',
+			'static_info_tables' => '2.3.0-2.3.99',
+			'php' => '5.3.0-5.3.99',
+			'typo3' => '4.5-4.7.99',
 		),
 		'conflicts' => array(
 		),
