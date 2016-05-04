ac0e69771592e5bd5dd5f9e9d4e2ae9959d38932
Manuel Selbach <manuel.selbach@udg.de>
2016-05-04 10:52:29 +0200

    [TASK] set code style to PRS2

--- ext_tables.php
+++ ext_tables.php
@@ -1,42 +1,46 @@
 <?php
-if (!defined ('TYPO3_MODE')) {
-	die ('Access denied.');
+if (!defined('TYPO3_MODE')) {
+    die('Access denied.');
 }
-$tablesAdditionalFields = array (
-	'static_countries' => array (
-		'cn_short_en' => 'cn_short_pl',
-		'cn_official_name_en' => 'cn_official_name_pl',
-		'cn_capital' => 'cn_capital_pl',
-	),
-	'static_country_zones' => array (
-		'zn_name_en' => 'zn_name_pl',
-	),
-	'static_currencies' => array (
-		'cu_name_en' => 'cu_name_pl',
-		'cu_sub_name_en' => 'cu_sub_name_pl',
-	),
-	'static_languages' => array (
-		'lg_name_en' => 'lg_name_pl',
-	),
-	'static_territories' => array (
-		'tr_name_en' => 'tr_name_pl',
-	),
-);
+$tablesAdditionalFields = [
+    'static_countries' => [
+        'cn_short_en' => 'cn_short_pl',
+        'cn_official_name_en' => 'cn_official_name_pl',
+        'cn_capital' => 'cn_capital_pl',
+    ],
+    'static_country_zones' => [
+        'zn_name_en' => 'zn_name_pl',
+    ],
+    'static_currencies' => [
+        'cu_name_en' => 'cu_name_pl',
+        'cu_sub_name_en' => 'cu_sub_name_pl',
+    ],
+    'static_languages' => [
+        'lg_name_en' => 'lg_name_pl',
+    ],
+    'static_territories' => [
+        'tr_name_en' => 'tr_name_pl',
+    ],
+];
 
-$extensionResourcesLanguagePath = 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_db.xlf:'; 
+$extensionResourcesLanguagePath = 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_db.xlf:';
 
 foreach ($tablesAdditionalFields as $table => $additionalFields) {
-	if (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) < 6001000) {
-		\TYPO3\CMS\Core\Utility\GeneralUtility::loadTCA($table);
-	}
-	foreach ($additionalFields as $sourceField => $destField) {
-		$additionalColumns = array();
-		$additionalColumns[$destField] = $GLOBALS['TCA'][$table]['columns'][$sourceField];
-		$additionalColumns[$destField]['label'] = $extensionResourcesLanguagePath . ':' . $table . '_item.' . $destField;
-		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns($table, $additionalColumns);
-		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes($table, $destField, '', 'after:' . $sourceField);
-	}
+    if (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) < 6001000) {
+        \TYPO3\CMS\Core\Utility\GeneralUtility::loadTCA($table);
+    }
+    foreach ($additionalFields as $sourceField => $destField) {
+        $additionalColumns = [];
+        $additionalColumns[$destField] = $GLOBALS['TCA'][$table]['columns'][$sourceField];
+        $additionalColumns[$destField]['label'] = $extensionResourcesLanguagePath . ':' . $table . '_item.' . $destField;
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns($table, $additionalColumns);
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+            $table,
+            $destField,
+            '',
+            'after:' . $sourceField
+        );
+    }
 }
 unset($additionalColumns);
 unset($tablesAdditionalFields);
-?>
