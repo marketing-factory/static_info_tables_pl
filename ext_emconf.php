26a0ed681e188a870281055526aa8d82c5305e6c
Ephraim Härer <ephraim@ephespage.de>
2019-10-08 23:10:04 +0200

    release 6.8.1 for TYPO3 9.5 LTS and 8.7

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,29 +1,31 @@
 <?php
+/***************************************************************
+ * Extension Manager/Repository config file for ext "static_info_tables_pl".
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = Array(
+$EM_CONF[$_EXTKEY] = [
     'title' => 'Static Info Tables (pl)',
     'description' => 'Polish (pl) language pack for the Static Info Tables providing localized names for countries, currencies and so on.',
     'category' => 'misc',
-    'version' => '6.8.0',
-    'priority' => '',
-    'loadOrder' => '',
-    'module' => '',
+    'version' => '6.8.1',
     'state' => 'stable',
-    'uploadfolder' => 0,
+    'uploadfolder' => false,
     'createDirs' => '',
     'modify_tables' => 'static_countries,static_languages,static_currencies,static_territories',
-    'clearcacheonload' => 1,
-    'lockType' => '',
-    'author' => 'Ephraim Härer',
-    'author_email' => 'ephraim.haerer@renolit.com',
+    'clearcacheonload' => false,
+    'author' => 'Simon Schmidt',
+    'author_email' => 'sfs@marketing-factory.de',
     'author_company' => 'jambage.com',
-    'constraints' => array(
-        'depends' => array(
+    'constraints' => [
+        'depends' => [
             'typo3' => '8.7.0-9.5.99',
             'static_info_tables' => '6.7.3-6.7.99',
-        ),
-        'conflicts' => array(),
-        'suggests' => array(),
-    ),
-);
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
 
--- ext_localconf.php
+++ ext_localconf.php
@@ -3,6 +3,6 @@ defined('TYPO3_MODE') || die();
 
 call_user_func(function ($extKey) {
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
-        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extKey . '/Configuration/TypoScript/Extbase/setup.txt">'
+        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extKey . '/Configuration/TypoScript/Extbase/setup.typoscript">'
     );
 }, 'static_info_tables_pl');
